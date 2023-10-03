9969c3b23dda4bd76fa2094f92b272265040d920
Vincent Guillemot <vincent.guillemot@pasteur.fr>
2023-10-03 13:52:17 +0200

    First commit

---
+++ R/debuter.R
@@ -0,0 +1,4 @@
+#' Package pour la formation "Débuter en R"
+#' Contient les vignettes (les slides de cours), le jeu de données "fruits", le jeu de données "nutrimenu"  et les tutoriels. https://vguillemot.github.io/debuter/
+#' @keywords internal
+"_PACKAGE"
